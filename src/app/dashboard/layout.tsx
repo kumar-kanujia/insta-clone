1c0c42ad51bc2f9125e8bee2561f4eaba471b38a
Kumar <70059467+kumar-kanujia@users.noreply.github.com>
2023-12-10 12:21:34 +0530

    initial project

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import SideNav from "@/components/SideNav";
+import { ReactNode } from "react";
+
+const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex h-screen relative flex-col md:flex-row md:overflow-hidden">
+      <aside className="w-20 flex-none lg:w-64 md:border-r">
+        <SideNav />
+      </aside>
+      <main className="flex-grow mt-12 md:mt-0 flex-1 w-full md:overflow-y-auto sm:p-6 md:p-12 max-w-7xl mx-auto">
+        {children}
+      </main>
+    </div>
+  );
+};
+export default DashboardLayout;
---
+++ src/components/SideNav.tsx
@@ -0,0 +1,4 @@
+const SideNav = () => {
+  return <div>SideNav</div>;
+};
+export default SideNav;
